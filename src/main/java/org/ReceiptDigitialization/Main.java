4b3aa8048d5126df54d1ebe614c75a1cf88bb5aa
Serhat Kumas <serhatkumas@outlook.com>
2023-01-28 03:16:27 +0300

    First version of jpg receipt module is developed

--- src/main/java/org/ReceiptDigitialization/Main.java
+++ src/main/java/org/ReceiptDigitialization/Main.java
@@ -1,6 +1,7 @@
 package org.ReceiptDigitialization;
 
 import org.ReceiptDigitialization.Models.*;
+import org.ReceiptDigitialization.ReceiptDigitizers.ImageDigitizer;
 import org.ReceiptDigitialization.ReceiptDigitizers.PdfDigitizer;
 
 import java.util.Date;
@@ -11,6 +12,7 @@ public class Main {
     public static void main(String[] args) {
 
         PdfDigitizer pdf = new PdfDigitizer();
+        ImageDigitizer img = new ImageDigitizer();
         List<Billable> billableList = new LinkedList<>();
         billableList.add(new Billable("Front-ent Development", 6, 15000));
         billableList.add(new Billable("Back-ent Development", 3, 20000));
@@ -20,6 +22,7 @@ public class Main {
         Company comp = new Company("Paradox Software Ltd. Sti", "Babacan sokak no : 37/10 Urla,Izmir", "05516217943", "paradoxsoftware@gmail.com");
         Person person_example = new Person("Sabri Tan","Osmangazi/Bursa","05536745698");
         pdf.digitizeReceipt(new Receipt(12345678910L,"Software Service Receipt", new Date(), billableList,0.18, comp, person_example));
+        img.digitizeReceipt(new Receipt(12345678910L,"Software Service Receipt", new Date(), billableList,0.18, comp, person_example));
 
     }
 }
--- src/main/java/org/ReceiptDigitialization/ReceiptDigitizers/ImageDigitizer.java
+++ src/main/java/org/ReceiptDigitialization/ReceiptDigitizers/ImageDigitizer.java
@@ -1,10 +1,86 @@
 package org.ReceiptDigitialization.ReceiptDigitizers;
 
+import org.ReceiptDigitialization.Models.Billable;
+import org.ReceiptDigitialization.Models.Company;
+import org.ReceiptDigitialization.Models.Person;
 import org.ReceiptDigitialization.Models.Receipt;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
 
 public class ImageDigitizer implements IDigitizer{
     @Override
     public void digitizeReceipt(Receipt receipt) {
         //https://examples.javacodegeeks.com/desktop-java/imageio/create-image-file-from-graphics-object/
+
+        int width = 500;
+        int height = 800;
+        double total_without_tax = 0;
+        int line_counter = 400;
+
+        // Constructs a BufferedImage of one of the predefined image types.
+        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        // Create a graphics which can be used to draw into the buffered image
+        Graphics2D g2d = bufferedImage.createGraphics();
+
+        // fill all the image with white
+        g2d.setColor(Color.white);
+        g2d.fillRect(0, 0, width, height);
+
+        // create a string with yellow
+        g2d.setColor(Color.black);
+        g2d.drawString("Receipt id : "+receipt.getReceipt_id(),50, 120);
+        g2d.drawString("Receipt title : "+receipt.getReceipt_title(),50, 140);
+        g2d.drawString("Receipt date : " + java.time.LocalDate.now(),50, 160);
+        g2d.drawString("-----------------------------------------------------",50, 180);
+        g2d.drawString("Company Name : " + receipt.getCompany_name(),50, 200);
+        g2d.drawString("Company Address : " + receipt.getCompany_address(),50, 220);
+        g2d.drawString("Company Phone : " + receipt.getCompany_phone(),50, 240);
+        g2d.drawString("Company Email : "+receipt.getCompany_email(),50, 260);
+        g2d.drawString("-----------------------------------------------------",50, 280);
+        g2d.drawString("Billed to Name : " + receipt.getPerson_name(),50, 300);
+        g2d.drawString("Billed to Address : " + receipt.getPerson_address(),50, 320);
+        g2d.drawString("Billed to Phone : " + receipt.getPerson_phone(),50, 340);
+        g2d.drawString("-----------------------------------------------------",50, 360);
+        g2d.drawString("Billable Content | Billable Quantity | Billable Per Price | Billable Total Price ",50, 380);
+        for(int i = 0; i < receipt.getBillable_list().size(); i++) {
+            Billable bill = receipt.getBillable_list().get(i);
+            g2d.drawString(bill.getBillable_substance()+" | "+bill.getBillable_quantity()+" | "+bill.getPrice_per_billable()+" | "+bill.getTotal_billable_price(),50, line_counter);
+            line_counter += 20;
+            total_without_tax += bill.getTotal_billable_price();
+        }
+        g2d.drawString("-----------------------------------------------------",50, line_counter);
+        line_counter += 20;
+        g2d.drawString("Total Price (No Tax) : " + total_without_tax,50, line_counter);
+        line_counter += 20;
+        g2d.drawString("Tax Rate : " + receipt.getTax_rate(),50, line_counter);
+        line_counter += 20;
+        g2d.drawString("Total Price : " + (total_without_tax + (total_without_tax * receipt.getTax_rate())),50, line_counter);
+
+        // Disposes of this graphics context and releases any system resources that it is using.
+        g2d.dispose();
+
+        // Save as PNG
+        File file = new File("receipt.png");
+        try {
+            ImageIO.write(bufferedImage, "png", file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Save as JPEG
+       // file = new File("myimage.jpg");
+       // ImageIO.write(bufferedImage, "jpg", file);
+
     }
+
 }
